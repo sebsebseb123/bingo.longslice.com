cd361ddc470a7b8f8619a0a1ce69e7ef633b840c
Sebastian Lesch <sebastian.lesch@gmail.com>
2023-01-11 23:34:13 -0500

    still

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import resolve from '@rollup/plugin-node-resolve'
+import json from '@rollup/plugin-json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    json,
+    resolve({
+      browser: true,
+      preferBuiltins: false,
+      dedupe: ['vite'],
+    }),
+  ],
   define: {
     global: 'window',
   },
